c23fbfeb405e6d414fa9908be056d5eeb61c8bac
Renan <renanpellegrini@outlook.com>
2023-05-28 15:16:08 -0300

    commit inicial

---
+++ main.js
@@ -0,0 +1,19 @@
+$(document).ready(function() {
+    
+    $("#devFullStack").click(function() {
+        $("#listaDev").slideToggle();
+    });
+
+    $("#fotografia").click(function() {
+        $("#listaFotografia").slideToggle();
+    });
+
+    $("#pessoal").click(function() {
+        $("#listaPessoal").slideToggle();
+    });
+
+    $("#stillus").click(function() {
+        $("#listaStillus").slideToggle();
+    });
+
+});
