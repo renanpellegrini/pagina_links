ebdfe7f52274f21daea36e545f86526b6fb392ca
Renan <renanpellegrini@outlook.com>
2023-05-28 20:04:16 -0300

    adicionando transições

--- main.js
+++ main.js
@@ -1,18 +1,26 @@
 $(document).ready(function() {
     
+    function hideOtherSublists(except){
+        $('.mainList').not(except).slideUp();
+    }
+
     $("#devFullStack").click(function() {
+        hideOtherSublists('#listaDev');
         $("#listaDev").slideToggle();
     });
 
     $("#fotografia").click(function() {
+        hideOtherSublists('#listaFotografia');
         $("#listaFotografia").slideToggle();
     });
 
     $("#pessoal").click(function() {
+        hideOtherSublists('#listaPessoal');
         $("#listaPessoal").slideToggle();
     });
 
     $("#stillus").click(function() {
+        hideOtherSublists('#listaStillus');
         $("#listaStillus").slideToggle();
     });
 
